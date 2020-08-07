11cdb8b3eb4a9130f0306a66509f9a65d2207b17
waterlove88 <poto1988@naver.com>
2020-08-08 02:27:11 +0900

    init

---
+++ src/main/java/com/waterlove88/luckti/LucktiApplication.java
@@ -0,0 +1,13 @@
+package com.waterlove88.luckti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LucktiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LucktiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/waterlove88/luckti/LucktiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waterlove88.luckti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LucktiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
